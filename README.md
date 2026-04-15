f29ff4ab0bfc7cf68a20ee3f4fc5d306d32fba3d
Surendra26 <yanamadalasurendra@gmai.com>
2026-04-15 17:40:49 +0530

    fix folder structure

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Sharath Kumar Porubothula Portfolio
+
+Welcome to my portfolio! Explore a collection of my projects and experiences in Web Development. I'm passionate about using technology to solve real-world problems.
+
+## Projects
+
+Browse through my projects to see examples of my work, including:
+- **Portfolio**
+- **Voxly**
+
+## Skills
+
+I'm proficient in:
+- Languages: Python, JavaScript, HTML, CSS
+- Database: SQL Server
+- Tools & Technologies: GitHub, Vercel
+
+## Contact
+
+Reach out to me at [psharathkumar21@gmail.com](mailto:psharathkumar21@gmail.com) to discuss collaboration opportunities or learn more about my work.
+
---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+    var navUl = document.getElementById('navUl');
+    navUl.classList.toggle('active');
+}
+
+// const text = document.querySelector(".text.sec-text");
+
+// const textLoad = () => {
+//     setTimeout(() => {
+//         text.textContent = "Web Developer";
+//         setTimeout(() => {
+//             text.textContent = "Frontend Developer";
+//             setTimeout(() => {
+//                 text.textContent = "Backend Developer";
+//             }, 4000);
+//         }, 4000);
+//     }, 0);
+// };
+
+// textLoad();
+// setInterval(textLoad, 12000);
+
+// Function to check if element is in viewport
+
